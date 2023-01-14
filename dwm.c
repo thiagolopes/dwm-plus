ffdacfe303993059e9832f97ca059386e75bfcec
thiagolopes <thiagolopes@protonmail.com>
2023-01-14 00:51:26 -0300

    Moveout code from fancy

--- dwm.c
+++ dwm.c
@@ -699,11 +699,15 @@ dirtomon(int dir)
 void
 drawbar(Monitor *m)
 {
-        int x, w, tw = 0, mw, ew = 0;
+        int x, w, tw = 0;
 	int boxs = drw->fonts->h / 9;
 	int boxw = drw->fonts->h / 6 + 2;
-	unsigned int i, occ = 0, urg = 0, n = 0;
+	unsigned int i, occ = 0, urg = 0;
 	Client *c;
+
+        /* fancy bar */
+        int mw, ew = 0;
+        unsigned int n = 0;
 
 	if (!m->showbar)
 		return;
