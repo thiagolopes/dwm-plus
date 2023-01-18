8c0f7e9e08406c0b7b3922752b450365c2338d40
thiagolopes <thiagolopes@protonmail.com>
2023-01-18 14:18:47 -0300

    Implement bar height

--- dwm.c
+++ dwm.c
@@ -1842,7 +1842,7 @@ setup(void)
 	if (!drw_fontset_create(drw, fonts, LENGTH(fonts)))
 		die("no fonts could be loaded.");
 	lrpad = drw->fonts->h;
-	bh = drw->fonts->h + 2;
+	bh = drw->fonts->h + bar_height;
 	updategeom();
 	/* init atoms */
 	utf8string = XInternAtom(dpy, "UTF8_STRING", False);
