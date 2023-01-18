b6e3306cab0d1e14863a1d9367278cfae7fbcc4e
thiagolopes <thiagolopes@protonmail.com>
2023-01-18 13:57:22 -0300

    Implement cool start

--- config.def.h
+++ config.def.h
@@ -23,6 +23,20 @@ static const char *colors[][3]      = {
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
 };
 
+static const char *const autostart[] = {
+        "dunst ", NULL,
+        "nitrogen", "--restore", NULL,
+        "redshift ", "-l -23:-47", NULL,
+        "picom", NULL,
+        "natural-scroll", NULL,
+        "octoxbps-notifier", NULL,
+        "xset", "r rate 300 50", NULL,
+        "autorandr", "--change 144hz", NULL,
+        "transmission-gtk ", "-m", NULL,
+        "copyq", NULL,
+	NULL
+};
+
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
